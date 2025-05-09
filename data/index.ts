61b5f6d4091fd4f434714758b5db53e7556f0d78
Bhuwan Chhimal <118724329+BhuwanChhimal@users.noreply.github.com>
2025-05-09 08:46:30 +0545

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -71,7 +71,15 @@ export const navItems = [
   
   export const projects = [
         {
-      id: 0,
+      id: 1,
+      title: "Guess the anime character",
+      des: "A fun game where you have to correctly guess the anime character.",
+      img: "/gtac.webp",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg","/mongo.svg","/express.svg"],
+      link: "https://chatify-fk94.onrender.com",
+    },
+        {
+      id: 2,
       title: "Chatify",
       des: "A online web chat-application to have fun converstation with your friends.",
       img: "/chatify.webp",
